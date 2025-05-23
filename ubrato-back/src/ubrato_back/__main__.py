5b7ab0becb9c100349031bdbd6f54e1783b37ffc
SamWarden <SamWardenSad@gmail.com>
2025-05-23 16:12:40 +0300

    feat(back): configure ubrato_back launch

--- ubrato-back/src/ubrato_back/__main__.py
+++ ubrato-back/src/ubrato_back/__main__.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from typing import Any, AsyncGenerator
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
@@ -100,3 +101,11 @@ app.add_exception_handler(
     auth_exception_handler,  # type: ignore
 )
 app.add_exception_handler(Exception, internal_exception_hander)
+
+
+def main() -> None:
+    uvicorn.run(app)
+
+
+if __name__ == "__main__":
+    main()
